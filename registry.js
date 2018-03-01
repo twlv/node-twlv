a1f3b355e6b213515d276a21bbfd58a7de6e26bf
Ganesha <reekoheek@gmail.com>
2018-03-01 23:38:47 +0700

    Fix bug hanging registry because unfinished task even though registry already stopped

--- registry.js
+++ registry.js
@@ -6,6 +6,7 @@ class Registry {
   constructor () {
     this.finders = [];
     this.peers = [];
+    this.tasks = [];
   }
 
   addFinder (finder) {
@@ -13,10 +14,13 @@ class Registry {
   }
 
   up () {
+    this.tasks = [];
     return Promise.all(this.finders.map(finder => finder.up()));
   }
 
   down () {
+    this.tasks.forEach(task => task.reject(new Error('Registry down')));
+    this.tasks = [];
     return Promise.all(this.finders.map(finder => finder.down()));
   }
 
@@ -27,7 +31,27 @@ class Registry {
     }
 
     let peerInfo = await new Promise(async (resolve, reject) => {
-      let _t = setTimeout(() => reject(new Error('Find timeout')), timeout);
+      let task = { resolve, reject };
+      this.tasks.push(task);
+
+      let _remove = () => {
+        let index = this.tasks.indexOf(task);
+        if (index !== -1) {
+          this.tasks.splice(index, 1);
+        }
+      };
+
+      let _reject = err => {
+        _remove();
+        reject(err);
+      };
+
+      let _resolve = val => {
+        _remove();
+        resolve(val);
+      };
+
+      let _t = setTimeout(() => _reject(new Error('Find timeout')), timeout);
 
       await Promise.all(this.finders.map(async finder => {
         try {
@@ -37,14 +61,14 @@ class Registry {
           }
 
           clearTimeout(_t);
-          resolve(peerInfo);
+          _resolve(peerInfo);
         } catch (err) {
           console.warn('Finder caught error', err);
         }
       }));
 
       clearTimeout(_t);
-      reject(new Error('Peer not found'));
+      _reject(new Error('Peer not found'));
     });
 
     return this.put(peerInfo);
